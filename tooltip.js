42e238ec96d70e9c93b67f34f3fdc00a8f1ed94c
conorjamesrogers <conorjames.rogers@gmail.com>
2018-06-04 22:33:47 -0700

    beginnings of tooltips

---
+++ tooltip.js
@@ -0,0 +1,28 @@
+function tooltip(d, xPosition, yPosition)
+{
+//    var xPosition = parseFloat(d3.event.clientX);
+//    var yPosition = parseFloat(d3.event.clientY-50);
+    d3.select(".tooltip").classed("hidden", false);
+
+    var tip = d3.select('.tooltip')
+            .style("left", (xPosition) + "px")
+            .style("top", (yPosition) + "px");
+    
+    tip.transition()
+          .duration(100)
+          .style("opacity", .9);
+        tip.select(".nickname")            
+            .text(d.nickname);
+        tip.select('.sci_name')
+            .text(d.sci_name);
+        tip.select('.soil')
+            .text('Soil Moisture: '+d.soil_ind);
+        tip.select('.sun')
+            .text('Sun-Comfort: '+d.sun);
+        tip.select('.water')
+            .text('Watering Amounts: '+d.water);
+    
+//    tip.style("opacity",'.9');
+    
+    console.log(d);
+}
