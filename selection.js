d855dc33d7cc025361b3575e801d212e7da68bff
conorjamesrogers <gnomy12000@gmail.com>
2018-06-08 16:48:59 -0700

    forgot to add ids to checkboxes

--- selection.js
+++ selection.js
@@ -5,24 +5,48 @@ function select_handler(svg, flowers, hung, dogs, cats){
             console.log("clicked flowers");
 
             flowers = !flowers;
+        
+            if(flowers==false){
+                
+                
+            }
         });
     d3.select('#hung')
         .on("click",function(){
             console.log("clicked");
 
             hung = !hung;
+            
+            if(hung==false){
+                
+               
+            }
         });
     d3.select('#dogs')
         .on("click",function(){
             console.log("clicked ");
 
             dogs = !dogs;
+            console.log(dogs);
+
+            
+            if(dogs==false){
+                nodes.map(function(d){
+//                    console.log(d.toxic_dogs);
+                });
+               
+            }
         });
     d3.select('#cats')
         .on("click",function(){
             console.log("clicked");
 
-            cats = !cats;            
+            cats = !cats;
+            
+            if(cats==false){
+                
+               
+            }
         });
     
     console.log("nodessss:",nodes)
